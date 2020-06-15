0a43f09f491ad6934e271d02b68d42802b16cc8d
Andy1407 <a.malinkin@lyceum1557.ru>
2020-06-15 18:20:36 +0300

    config

--- app/__init__.py
+++ app/__init__.py
@@ -13,6 +13,8 @@ from flask_babel import Babel
 from flask_babel import lazy_gettext as _l
 from app import cli
 from elasticsearch import Elasticsearch
+from redis import Redis
+import rq
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -37,6 +39,8 @@ def create_app(config_class=Config):
     moment.init_app(app)
     babel.init_app(app)
     app.elasticsearch = Elasticsearch([app.config['ELASTICSEARCH_URL']]) if app.config['ELASTICSEARCH_URL'] else None
+    app.redis = Redis.from_url(app.config['REDIS_URL'])
+    app.task_queue = rq.Queue('microblog-tasks', connection=app.redis)
 
     from app.errors import bp as errors_bp
     app.register_blueprint(errors_bp)
