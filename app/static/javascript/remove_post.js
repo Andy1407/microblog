1cdb3e3af31d2e00be97c2de43d192c0c90b8236
Andy1407 <a.malinkin@lyceum1557.ru>
2020-07-04 17:14:54 +0300

    debug

--- app/static/javascript/remove_post.js
+++ app/static/javascript/remove_post.js
@@ -1,12 +1,15 @@
 function remove_post(id) {
-            $.post('/remove_post', {
-                'id': id
-            }).done(function (response) {
-                if (response['status'] === 'error') {
-                    alert(response['response'])
-                } else if (response['status'] === 'successfully') {
-                    let post = document.querySelectorAll(`#post-${response['response']}`)
-                    post.forEach(post => post.remove())
-                }
-            })
-        }
+    let remove = confirm('Do you want to remove this post?')
+    if (remove) {
+        $.post('/remove_post', {
+            'id': id
+        }).done(function (response) {
+            if (response['status'] === 'error') {
+                alert(response['response'])
+            } else if (response['status'] === 'successfully') {
+                let post = document.querySelectorAll(`#post-${response['response']}`)
+                post.forEach(post => post.remove())
+            }
+        })
+    }
+}
