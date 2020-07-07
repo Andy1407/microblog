da54bbe0789147a6d898ea44689917786eeebe49
Andy1407 <a.malinkin@lyceum1557.ru>
2020-07-07 22:30:47 +0300

    debug

--- app/static/javascript/remove_post.js
+++ app/static/javascript/remove_post.js
@@ -1,10 +1,13 @@
 function remove_post(id) {
     let current_post = $(`#post-${id}`)
 
+    current_post.hide()
+
     $.post('/remove_post', {
         'id': id
     }).done(function (response) {
         if (response['status'] === 'error') {
+            current_post.show()
             alert(response['response'])
         } else if (response['status'] === 'successfully') {
             current_post.remove()
