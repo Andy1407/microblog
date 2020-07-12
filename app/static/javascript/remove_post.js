a5b3510a9d9a100f1bbef98dce0d17c9448fc077
Andy1407 <a.malinkin@lyceum1557.ru>
2020-07-12 18:21:48 +0300

    debug

--- app/static/javascript/remove_post.js
+++ app/static/javascript/remove_post.js
@@ -1,59 +1,74 @@
 $(document).ready(function () {
     let a = setTimeout(function () {
         let elem = Array.from(document.getElementsByClassName('alert alert-info'))
+
+        let div = Array.from(document.getElementsByClassName('alerts'))[0]
+        let content = Array.from(document.getElementById('content').children)
+
         elem.forEach(elem => {
-            elem.remove()
+            div.appendChild(elem)
+            let anim = elem.animate({transform: `translate(0px, -${div.clientHeight}px)`}, 400)
+            anim.addEventListener('finish', function () {
+                elem.remove()
+            })
         })
 
-    }, 10000)
-})
 
-function remove_post(id, message) {
-    let current_post = $(`#thisPost${id}`)
-    let current_post2 = document.getElementById(`post-${id}`)
-    let posts = Array.from(document.getElementById('posts').children)
+            content.forEach(content => {
+                if (content.className !== "alerts") {
+                    content.animate({transform: `translate(0px, -${div.clientHeight}px)`}, 400)
+                }
+            })
 
-    document.getElementById(`thisPost${id}`).style.overflow = 'hidden'
+        }, 2000)
+    })
 
-    animation(current_post2, posts, id, function () {
-        current_post.hide()
+    function remove_post(id, message) {
+        let current_post = $(`#thisPost${id}`)
+        let current_post2 = document.getElementById(`post-${id}`)
+        let posts = Array.from(document.getElementById('posts').children)
 
-        $.post('/remove_post', {
-            'id': id
-        }).done(function (response) {
-            if (response['status'] === 'error') {
-                current_post.show()
-                alert(response['response'])
-            } else if (response['status'] === 'successfully') {
-                current_post.remove()
-                toast(message, undefined, undefined, '#323232', '#ffffff')
+        document.getElementById(`thisPost${id}`).style.overflow = 'hidden'
+
+        animation(current_post2, posts, id, function () {
+            current_post.hide()
+
+            $.post('/remove_post', {
+                'id': id
+            }).done(function (response) {
+                if (response['status'] === 'error') {
+                    current_post.show()
+                    alert(response['response'])
+                } else if (response['status'] === 'successfully') {
+                    current_post.remove()
+                    toast(message, undefined, undefined, '#323232', '#ffffff')
+                }
+            })
+        })
+
+
+    }
+
+    function animation(current_post, posts, id, onFinish = function () {
+    }) {
+
+        let anim = current_post.animate({transform: `translate(0px, -${current_post.clientHeight}px)`}, 400)
+
+        anim.addEventListener('finish', onFinish)
+
+        let afterPost = false
+        let lastPost = current_post
+        posts.forEach(post => {
+            if (afterPost && lastPost.style.display !== 'none' && $(post).is(":visible")) {
+                let distance = post.getBoundingClientRect().top - lastPost.getBoundingClientRect().top
+                lastPost = post
+                let animate = post.animate({transform: `translate(0px, -${distance}px)`}, 400)
+
+            }
+            if (post.id === `thisPost${id}`) {
+                afterPost = true
             }
         })
-    })
 
 
-}
-
-function animation(current_post, posts, id, onFinish = function () {
-}) {
-
-    let anim = current_post.animate({transform: `translate(0px, -${current_post.clientHeight}px)`}, 400)
-
-    anim.addEventListener('finish', onFinish)
-
-    let afterPost = false
-    let lastPost = current_post
-    posts.forEach(post => {
-        if (afterPost && lastPost.style.display !== 'none' && $(post).is(":visible")) {
-            let distance = post.getBoundingClientRect().top - lastPost.getBoundingClientRect().top
-            lastPost = post
-            let animate = post.animate({transform: `translate(0px, -${distance}px)`}, 400)
-
-        }
-        if (post.id === `thisPost${id}`) {
-            afterPost = true
-        }
-    })
-
-
-}
+    }
