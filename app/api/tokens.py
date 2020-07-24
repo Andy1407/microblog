106dcbb316d5c334bf54b1843a781774d3e70d29
Andy1407 <a.malinkin@lyceum1557.ru>
2020-07-24 18:15:38 +0300

    debug

--- app/api/tokens.py
+++ app/api/tokens.py
@@ -3,14 +3,17 @@ from app import db
 from app.api import bp
 from app.api.auth import basic_auth
 from app.api.auth import token_auth
+from app.models import User
 
 
 @bp.route('/tokens', methods=['POST'])
 @basic_auth.login_required
 def get_token():
+    username = basic_auth.username()
+    user = User.query.filter_by(username=username).first()
     token = g.current_user.get_token()
     db.session.commit()
-    return jsonify({'token': token})
+    return jsonify({'token': token, 'id': user.id})
 
 
 @bp.route('/tokens', methods=['DELETE'])
